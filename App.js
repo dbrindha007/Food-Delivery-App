9442c551a6271b1202e38188db45d8ca2cda8311
dbrindha007 <dbrindha007@gmail.com>
2025-09-16 07:49:17 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/*
+*  Nested React Elements using React.createElement
+*/
+
+const parent = React.createElement('div', { id: "parent" },
+    [React.createElement('div', { className: "child" },
+        React.createElement('h1', null, 'Hello World'),
+        React.createElement('p', null, 'This is a paragraph inside a child div.'))],
+    [
+        React.createElement('div', { className: "child2" },
+            React.createElement('h1', null, 'Hello World2'),
+            React.createElement('p', null, 'This is a paragraph inside a child div.2'))]
+);
+console.log(parent); //Return Object {type: "h1", props: {…}, key: null, ref: null, _owner: null, …}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(root);
+// Render the React element into the root div
+root.render(parent); 
+
+
+
