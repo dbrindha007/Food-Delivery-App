1a684dc9d54a6bb25dd9e99dcad5ac8b3c8447d3
dbrindha007 <dbrindha007@gmail.com>
2025-09-23 19:35:50 +0530

    Using npm installed parcel into our app

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /*
 *  Nested React Elements using React.createElement
 */
 
 const parent = React.createElement('div', { id: "parent" },
-    [React.createElement('div', { className: "child" },
+    [React.createElement('div', { key: 1, className: "child" },
         React.createElement('h1', null, 'Hello World'),
         React.createElement('p', null, 'This is a paragraph inside a child div.'))],
     [
-        React.createElement('div', { className: "child2" },
+        React.createElement('div', {key: 2  , className: "child2" },
             React.createElement('h1', null, 'Hello World2'),
             React.createElement('p', null, 'This is a paragraph inside a child div.2'))]
 );
